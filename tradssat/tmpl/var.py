f4b264b48a963d39891ae9b4e976a0e135e1008e
julienmalard <julien.malard@mail.mcgill.ca>
2018-11-03 15:16:44 -0400

    Refactored variable limits to new Numeric type

--- tradssat/tmpl/var.py
+++ tradssat/tmpl/var.py
@@ -1,10 +1,12 @@
+import numpy as np
+
+
 class Variable(object):
     type_ = None
 
-    def __init__(self, name, size, lims=None, spc=1, info=''):
+    def __init__(self, name, size, spc, info):
         self.name = name
         self.size = size
-        self.lims = lims
         self.info = info
         self.spc = spc
 
@@ -14,23 +16,59 @@ class Variable(object):
         else:
             return self._write(val)
 
+    def check_val(self, val):
+        raise NotImplementedError
+
     def _write(self, val):
         raise NotImplementedError
 
     def __str__(self):
         return str(self.name)
 
+
 class CharacterVar(Variable):
     type_ = str
 
-    def __init__(self, name, size, lims=None, spc=1, info=''):
-        super().__init__(name, size, lims, spc, info)
+    def __init__(self, name, size, spc=1, info=''):
+        super().__init__(name, size, spc, info)
+
+    def check_val(self, val):
+        if len(val) > self.size:
+            raise ValueError(
+                'Value "{val}" is longer than {sz} characters for variable {name}.'
+                    .format(val=val, sz=self.size, name=self)
+            )
 
     def _write(self, val):
         return val.rjust(self.size + self.spc)
 
 
-class FloatVar(Variable):
+class NumericVar(Variable):
+
+    def __init__(self, name, size, lims, spc, info):
+
+        super().__init__(name, size, spc, info)
+
+        if lims is None:
+            lims = (-np.inf, np.inf)
+        lims = (-np.inf if lims[0] is None else lims[0], np.inf if lims[1] is None else lims[1])
+
+        self.lims = lims
+
+    def check_val(self, val):
+        val = np.array(val)
+        out = np.where(np.logical_or(np.less(val, self.lims[0]), np.greater(val, self.lims[1])))
+        if len(out):
+            vals_out = val[out]
+            raise ValueError(
+                'Value {val} is not in range {rng} for variable {name}.'.format(val=vals_out, name=self, rng=self.lims)
+            )
+
+    def _write(self, val):
+        raise NotImplementedError
+
+
+class FloatVar(NumericVar):
     type_ = float
 
     def __init__(self, name, size, dec, lims=None, spc=1, info=''):
@@ -41,7 +79,7 @@ class FloatVar(Variable):
         return '{:{sz}.{dec}f}'.format(val, sz=self.size, dec=self.dec).rjust(self.spc + self.size)
 
 
-class IntegerVar(Variable):
+class IntegerVar(NumericVar):
     type_ = int
 
     def __init__(self, name, size, lims=None, spc=1, info=''):
