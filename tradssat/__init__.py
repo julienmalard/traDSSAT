4ac92bf27814d7e1ba460d28297c6ccc0f624a61
julienmalard <julien.malard@mail.mcgill.ca>
2018-11-09 17:42:28 -0500

    genotype files added

--- tradssat/__init__.py
+++ tradssat/__init__.py
@@ -1,5 +1,6 @@
 from .exper import ExpFile
 from .soil import SoilFile
 from .weather import WTHFile
+from .genotype import CULFile, ECOFile
 from .out import PlantGrowOut
 from .runs.input import DSSATRun
