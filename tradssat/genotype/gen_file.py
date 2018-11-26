82b65923295d932b156f50ad9f3a270ab588eafd
julienmalard <julien.malard@mail.mcgill.ca>
2018-11-26 14:02:51 -0500

    Refactor classes

--- tradssat/genotype/gen_file.py
+++ tradssat/genotype/gen_file.py
@@ -47,84 +47,74 @@ from .vars_.whcer import cul_vars_WHCER, eco_vars_WHCER
 from .vars_.whcrp import cul_vars_WHCRP, eco_vars_WHCRP
 
 vars_gen = {
-    'ALFRM': {'cul': cul_vars_ALFRM, 'eco': eco_vars_ALFRM},
-    'BACER': {'cul': cul_vars_BACER, 'eco': eco_vars_BACER},
-    'BACRP': {'cul': cul_vars_BACRP, 'eco': eco_vars_BACRP},
-    'BHGRO': {'cul': cul_vars_BHGRO, 'eco': eco_vars_BHGRO},
-    'BMRFM': {'cul': cul_vars_BMFRM, 'eco': eco_vars_BMFRM},
-    'BMGRO': {'cul': cul_vars_BMGRO, 'eco': eco_vars_BMGRO},
-    'BNGRO': {'cul': cul_vars_BNGRO, 'eco': eco_vars_BNGRO},
-    'BRFRM': {'cul': cul_vars_BRFRM, 'eco': eco_vars_BRFRM},
-    'BRGRO': {'cul': cul_vars_BRGRO, 'eco': eco_vars_BRGRO},
-    'BSCER': {'cul': cul_vars_BSCER, 'eco': eco_vars_BSCER},
-    'CBGRO': {'cul': cul_vars_CBGRO, 'eco': eco_vars_CBGRO},
-    'CHGRO': {'cul': cul_vars_CHGRO, 'eco': eco_vars_CHGRO},
-    'CNGRO': {'cul': cul_vars_CNGRO, 'eco': eco_vars_CNGRO},
-    'COGRO': {'cul': cul_vars_COGRO, 'eco': eco_vars_COGRO},
-    'CPGRO': {'cul': cul_vars_CPGRO, 'eco': eco_vars_CPGRO},
-    'CSCAS': {'cul': cul_vars_CSCAS, 'eco': eco_vars_CSCAS},
-    'CSYCA': {'cul': cul_vars_CSYCA, 'eco': eco_vars_CSYCA},
-    'FBGRO': {'cul': cul_vars_FBGRO, 'eco': eco_vars_FBGRO},
-    'G0GRO': {'cul': cul_vars_G0GRO, 'eco': eco_vars_G0GRO},
-    'GBGRO': {'cul': cul_vars_GBGRO, 'eco': eco_vars_GBGRO},
-    'MLCER': {'cul': cul_vars_MLCER, 'eco': eco_vars_MLCER},
-    'MZCER': {'cul': cul_vars_MZCER, 'eco': eco_vars_MZCER},
-    'MZIXM': {'cul': cul_vars_MZIXM, 'eco': eco_vars_MZIXM},
-    'PIALO': {'cul': cul_vars_PIALO, 'eco': eco_vars_PIALO},
-    'PNGRO': {'cul': cul_vars_PNGRO, 'eco': eco_vars_PNGRO},
-    'PPGRO': {'cul': cul_vars_PPGRO, 'eco': eco_vars_PPGRO},
-    'PRGRO': {'cul': cul_vars_PRGRO, 'eco': eco_vars_PRGRO},
-    'PTSUB': {'cul': cul_vars_PTSUB, 'eco': eco_vars_PTSUB},
-    'RICER': {'cul': cul_vars_RICER, 'eco': eco_vars_RICER},
-    'RIORZ': {'cul': cul_vars_RIORZ, 'eco': eco_vars_RIORZ},
-    'SBGRO': {'cul': cul_vars_SBGRO, 'eco': eco_vars_SBGRO},
-    'SCCAN': {'cul': cul_vars_SCCAN, 'eco': eco_vars_SCCAN},
-    'SCCSP': {'cul': cul_vars_SCCSP, 'eco': eco_vars_SCCSP},
-    'SFGRO': {'cul': cul_vars_SFGRO, 'eco': eco_vars_SFGRO},
-    'SGCER': {'cul': cul_vars_SGCER, 'eco': eco_vars_SGCER},
-    'SUGRO': {'cul': cul_vars_SUGRO, 'eco': eco_vars_SUGRO},
-    'SWCER': {'cul': cul_vars_SWCER, 'eco': eco_vars_SWCER},
-    'TNARO': {'cul': cul_vars_TNARO},
-    'TMGRO': {'cul': cul_vars_TMGRO, 'eco': eco_vars_TMGRO},
-    'TRARO': {'cul': cul_vars_TRARO, 'eco': eco_vars_TRARO},
-    'VBGRO': {'cul': cul_vars_VBGRO, 'eco': eco_vars_VBGRO},
-    'WHAPS': {'cul': cul_vars_WHAPS, 'eco': eco_vars_WHAPS},
-    'WHCER': {'cul': cul_vars_WHCER, 'eco': eco_vars_WHCER},
-    'WHCRP': {'cul': cul_vars_WHCRP, 'eco': eco_vars_WHCRP},
+    'ALFRM': {'.cul': cul_vars_ALFRM, '.eco': eco_vars_ALFRM},
+    'BACER': {'.cul': cul_vars_BACER, '.eco': eco_vars_BACER},
+    'BACRP': {'.cul': cul_vars_BACRP, '.eco': eco_vars_BACRP},
+    'BHGRO': {'.cul': cul_vars_BHGRO, '.eco': eco_vars_BHGRO},
+    'BMRFM': {'.cul': cul_vars_BMFRM, '.eco': eco_vars_BMFRM},
+    'BMGRO': {'.cul': cul_vars_BMGRO, '.eco': eco_vars_BMGRO},
+    'BNGRO': {'.cul': cul_vars_BNGRO, '.eco': eco_vars_BNGRO},
+    'BRFRM': {'.cul': cul_vars_BRFRM, '.eco': eco_vars_BRFRM},
+    'BRGRO': {'.cul': cul_vars_BRGRO, '.eco': eco_vars_BRGRO},
+    'BSCER': {'.cul': cul_vars_BSCER, '.eco': eco_vars_BSCER},
+    'CBGRO': {'.cul': cul_vars_CBGRO, '.eco': eco_vars_CBGRO},
+    'CHGRO': {'.cul': cul_vars_CHGRO, '.eco': eco_vars_CHGRO},
+    'CNGRO': {'.cul': cul_vars_CNGRO, '.eco': eco_vars_CNGRO},
+    'COGRO': {'.cul': cul_vars_COGRO, '.eco': eco_vars_COGRO},
+    'CPGRO': {'.cul': cul_vars_CPGRO, '.eco': eco_vars_CPGRO},
+    'CSCAS': {'.cul': cul_vars_CSCAS, '.eco': eco_vars_CSCAS},
+    'CSYCA': {'.cul': cul_vars_CSYCA, '.eco': eco_vars_CSYCA},
+    'FBGRO': {'.cul': cul_vars_FBGRO, '.eco': eco_vars_FBGRO},
+    'G0GRO': {'.cul': cul_vars_G0GRO, '.eco': eco_vars_G0GRO},
+    'GBGRO': {'.cul': cul_vars_GBGRO, '.eco': eco_vars_GBGRO},
+    'MLCER': {'.cul': cul_vars_MLCER, '.eco': eco_vars_MLCER},
+    'MZCER': {'.cul': cul_vars_MZCER, '.eco': eco_vars_MZCER},
+    'MZIXM': {'.cul': cul_vars_MZIXM, '.eco': eco_vars_MZIXM},
+    'PIALO': {'.cul': cul_vars_PIALO, '.eco': eco_vars_PIALO},
+    'PNGRO': {'.cul': cul_vars_PNGRO, '.eco': eco_vars_PNGRO},
+    'PPGRO': {'.cul': cul_vars_PPGRO, '.eco': eco_vars_PPGRO},
+    'PRGRO': {'.cul': cul_vars_PRGRO, '.eco': eco_vars_PRGRO},
+    'PTSUB': {'.cul': cul_vars_PTSUB, '.eco': eco_vars_PTSUB},
+    'RICER': {'.cul': cul_vars_RICER, '.eco': eco_vars_RICER},
+    'RIORZ': {'.cul': cul_vars_RIORZ, '.eco': eco_vars_RIORZ},
+    'SBGRO': {'.cul': cul_vars_SBGRO, '.eco': eco_vars_SBGRO},
+    'SCCAN': {'.cul': cul_vars_SCCAN, '.eco': eco_vars_SCCAN},
+    'SCCSP': {'.cul': cul_vars_SCCSP, '.eco': eco_vars_SCCSP},
+    'SFGRO': {'.cul': cul_vars_SFGRO, '.eco': eco_vars_SFGRO},
+    'SGCER': {'.cul': cul_vars_SGCER, '.eco': eco_vars_SGCER},
+    'SUGRO': {'.cul': cul_vars_SUGRO, '.eco': eco_vars_SUGRO},
+    'SWCER': {'.cul': cul_vars_SWCER, '.eco': eco_vars_SWCER},
+    'TNARO': {'.cul': cul_vars_TNARO},
+    'TMGRO': {'.cul': cul_vars_TMGRO, '.eco': eco_vars_TMGRO},
+    'TRARO': {'.cul': cul_vars_TRARO, '.eco': eco_vars_TRARO},
+    'VBGRO': {'.cul': cul_vars_VBGRO, '.eco': eco_vars_VBGRO},
+    'WHAPS': {'.cul': cul_vars_WHAPS, '.eco': eco_vars_WHAPS},
+    'WHCER': {'.cul': cul_vars_WHCER, '.eco': eco_vars_WHCER},
+    'WHCRP': {'.cul': cul_vars_WHCRP, '.eco': eco_vars_WHCRP},
 }
 
 
-class CULFile(InpFile):
+class GenFile(InpFile):
+    ext = None  # type: str
+
+    def _get_var_info(self):
+        file = os.path.split(self.file)[1]
+
+        for gen, d_gen in vars_gen.items():
+            if file.startswith(gen):
+                try:
+                    return d_gen[self.ext.lower()]
+                except KeyError:
+                    break
+
+        raise ValueError(
+            'No {ext} variables defined for {f} cropping model.'.format(ext=self.ext, f=os.path.splitext(file)[0])
+        )
+
+
+class CULFile(GenFile):
     ext = '.CUL'
 
-    def _get_var_info(self):
-        file = os.path.split(self.file)[1]
 
-        for gen, d_gen in vars_gen.items():
-            if file.startswith(gen):
-                try:
-                    return d_gen['cul']
-                except KeyError:
-                    break
-
-        raise ValueError(
-            'No cultivar variables defined for {} cropping model.'.format(os.path.splitext(file)[0])
-        )
-
-
-class ECOFile(InpFile):
+class ECOFile(GenFile):
     ext = '.ECO'
-
-    def _get_var_info(self):
-        file = os.path.split(self.file)[1]
-
-        for gen, d_gen in vars_gen.items():
-            if file.startswith(gen):
-                try:
-                    return d_gen['eco']
-                except KeyError:
-                    break
-
-        raise ValueError(
-            'No ecotype variables defined for {} cropping model.'.format(os.path.splitext(file)[0])
-        )
