99d3517ac0d9988a973589fb8a0bc0fdf0cecfda
julienmalard <julien.malard@mail.mcgill.ca>
2018-11-27 17:15:24 -0500

    Progress on CropGro genetics

--- tradssat/genotype/vars_/bmgro.py
+++ tradssat/genotype/vars_/bmgro.py
@@ -1,5 +1,15 @@
 from tradssat.genotype.vars_.cropgro import cropgro_cul_vars, cropgro_eco_vars
+from tradssat.tmpl.var import FloatVar
 
 cul_vars_BMGRO = cropgro_cul_vars()
 
 eco_vars_BMGRO = cropgro_eco_vars()
+eco_vars_BMGRO.update({
+    FloatVar('RDRMT', 5, dec=3,
+             info='Relative dormancy sensitivity of this cultivar to daylength - partitioning (0-1)'),
+    FloatVar('RDRMG', 5, dec=3,
+             info='Relative dormancy sensitivity of this cultivar to daylength - photosynthesis (0-1)'),
+    FloatVar('RDRMM', 5, dec=3,
+             info='Relative dormancy sensitivity of this cultivar to daylength - mobilization (0-1)'),
+    FloatVar('RCHDP', 5, dec=3, info='Relative cold hardening potential (0-1)')
+})
