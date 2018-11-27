39358495b78edaaa216ba23fa5f046128a0fdae4
julienmalard <julien.malard@mail.mcgill.ca>
2018-11-27 17:15:24 -0500

    Progress on CropGro genetics

--- tradssat/genotype/vars_/cogro.py
+++ tradssat/genotype/vars_/cogro.py
@@ -1,5 +1,7 @@
 from tradssat.genotype.vars_.cropgro import cropgro_cul_vars, cropgro_eco_vars
+from tradssat.tmpl.var import CharacterVar
 
 cul_vars_COGRO = cropgro_cul_vars()
 
 eco_vars_COGRO = cropgro_eco_vars()
+eco_vars_COGRO.add(CharacterVar('KCAN', 5, info='Who knows'))
