a212071bf320dfe5a2ed43a7175cb74fa1320e6b
julienmalard <julien.malard@mail.mcgill.ca>
2019-12-31 13:09:26 -0500

    Fix doc build with transifex

--- docs/.tx/tx.sh
+++ docs/.tx/tx.sh
@@ -34,7 +34,6 @@ token =" > ~/.transifexrc
 
 update_translations() {
   make gettext
-  rm .tx/config
   sphinx-intl create-txconfig
   sphinx-intl update-txconfig-resources --transifex-project-name tradssat
 }
@@ -42,7 +41,7 @@ update_translations() {
 tx_push() {
   # Only run once, and only on $TX_BRANCH branch
   echo $TRAVIS_JOB_NUMBER | grep "\.1$"
-  if [ $? -eq 0 ]  && [ "َ$TRAVIS_BRANCH" == "$TX_BRANCH" ]
+  if [ $? -eq 0 ] && [ "$TRAVIS_BRANCH" = "$TX_BRANCH" ];
     then
       tx_init
       update_translations
@@ -51,9 +50,9 @@ tx_push() {
 }
 
 tx_pull() {
-  # Only run once, and only for $TX_TAG tag
+  # Only run once, and only on $TX_BRANCH branch
   echo $TRAVIS_JOB_NUMBER | grep "\.1$"
-  if [ $? -eq 0 ] && [ "$TRAVIS_TAG" = "$TX_TAG" ]
+  if [ $? -eq 0 ] && [ "$TRAVIS_BRANCH" = "$TX_BRANCH" ];
     then
       echo "pulling"
       tx_init
