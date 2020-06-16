c5ccb841752702b8fd3a493ff25297e00c841d8d
Fábio Seixas <flscosta94@gmail.com>
2020-06-16 09:53:08 -0300

    Run header variable (#22)
    * fix test_mgrs
    
    * sect argument was missing
    
    * fix tests directory
    
    * RUN as a header variable
    
    * small change in DSSATResult
    
    * small fix
    
    * RUN as a header variable
    
    * small change in DSSATResult
    
    * small fix
    
    * updated json files
    
    * More robust this way
    
    * Oops
    
    Co-authored-by: Julien Malard <julien.malard@mail.mcgill.ca>

--- tradssat/mgrs/out.py
+++ tradssat/mgrs/out.py
@@ -35,7 +35,7 @@ class DSSATResults(object):
             self._outfiles[out_class.filename] = None
         self._sumoutfile = None
 
-    def get_value(self, var, trt, t=None, at='YEAR DOY'):
+    def get_value(self, var, trt, t=None, at='YEAR DOY', run=None):
         """
         Returns the value (point or time-series) of a variable from a DSSAT run.
 
@@ -66,6 +66,11 @@ class DSSATResults(object):
         else:
             cond = None
 
+        if run is None:
+            sect = {'TREATMENT': trt}
+        else:
+            sect = {'TREATMENT': trt, 'RUN': run}
+
         for c, f in self._outfiles.items():
 
             if f is None:
@@ -76,8 +81,8 @@ class DSSATResults(object):
 
             if var in f.variables():
                 if t is None:
-                    return f.get_value(var, sect={'TREATMENT': trt})
-                return f.get_value(var, sect={'TREATMENT': trt}, cond=cond)
+                    return f.get_value(var, sect=sect)
+                return f.get_value(var, sect=sect, cond=cond)
 
         raise ValueError('Output variable "{}" could not be found in any output file.'.format(var))
 
